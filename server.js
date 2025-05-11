6ad832cdf57e4d79710934248d971e15545f19bb
Mawuli2026 <mawuliayikpa89@gmail.com>
2025-05-11 09:52:14 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/' , (req , res)  => {
+res.send('Hello Mawuli');
+
+});
+
+
+ 
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
