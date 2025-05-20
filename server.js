6c0b9572639726fad06803193c7927d8c1d8e751
Mawuli2026 <mawuliayikpa89@gmail.com>
2025-05-20 21:14:03 +0100

    commit post,put and delete

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const { initDb } = require('./db/connect');
 const contactRoutes = require('./routes/contact');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello Mawuli!');
 });
 
 // Contacts API route
