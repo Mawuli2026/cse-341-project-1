b00585759935b386b4b4945a960d2de2f09af963
Mawuli2026 <mawuliayikpa89@gmail.com>
2025-05-23 19:33:47 +0100

    Change http to https in swagger.js

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing contacts'
   },
   host: 'cse-341-project-1-kc2a.onrender.com', 
-  schemes: ['http']
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
